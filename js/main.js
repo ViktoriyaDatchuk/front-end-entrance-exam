69dc19cc0c7cd0a3b850780723ade2f51059aefa
Viktoriya Datchuk <grelada@tut.by>
2024-08-04 22:15:24 +0300

    feat: add animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+const editingElements = document.querySelectorAll('[contentEditable]');
+
+editingElements.forEach((elem) => {
+    elem.addEventListener('input', (e) => {
+        e.target.classList.add('animation');
+
+        e.target.addEventListener('animationend', (animationEvent) => {
+            if (animationEvent.animationName === 'text-animation') {
+                e.target.classList.remove('animation')
+            }
+        })
+    })
+})
 
