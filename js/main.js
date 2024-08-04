a94aafb8c941ab83b19cb575ab30ce9698a90e31
Viktoriya Datchuk <grelada@tut.by>
2024-08-05 01:40:26 +0300

    feat: add download CV

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 const editingElements = document.querySelectorAll('[contentEditable]');
+const downloadButton = document.querySelector('.downloadButton');
 
 editingElements.forEach((elem) => {
-    elem.addEventListener('input', (e) => {
-        e.target.classList.add('animation');
+  elem.addEventListener('input', (e) => {
+    e.target.classList.add('animation');
 
-        e.target.addEventListener('animationend', (animationEvent) => {
-            if (animationEvent.animationName === 'text-animation') {
-                e.target.classList.remove('animation')
-            }
-        })
+    e.target.addEventListener('animationend', (animationEvent) => {
+      if (animationEvent.animationName === 'text-animation') {
+        e.target.classList.remove('animation')
+      }
     })
+  })
 })
 
+downloadButton.addEventListener('click', () => {
+  const CV = document.querySelector('.wrapper');
+  const opt = {
+    margin: 0.5,
+    filename: 'CV.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { 
+      scale: 1
+     },
+    jsPDF: { 
+      unit: 'mm', 
+      format: 'a4', 
+      orientation: 'portrait',
+   }
+  }
+
+  html2pdf().set(opt).from(CV).save();
+})
