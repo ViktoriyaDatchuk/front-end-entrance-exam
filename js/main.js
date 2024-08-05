d8ff29ae3618dfecb747817f1915ba3c67228af8
Viktoriya Datchuk <grelada@tut.by>
2024-08-05 03:32:18 +0300

    feat: add Material Wave

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 const editingElements = document.querySelectorAll('[contentEditable]');
 const downloadButton = document.querySelector('.downloadButton');
 
-editingElements.forEach((elem) => {
-  elem.addEventListener('input', (e) => {
-    e.target.classList.add('animation');
-
-    e.target.addEventListener('animationend', (animationEvent) => {
-      if (animationEvent.animationName === 'text-animation') {
-        e.target.classList.remove('animation');
-      }
+const animationHandler = () => {
+  editingElements.forEach((elem) => {
+    elem.addEventListener('input', (e) => {
+      e.target.classList.add('animation');
+  
+      e.target.addEventListener('animationend', (animationEvent) => {
+        if (animationEvent.animationName === 'text-animation') {
+          e.target.classList.remove('animation');
+        }
+      });
     });
   });
-});
+}
 
-downloadButton.addEventListener('click', () => {
-  const CV = document.querySelector('.wrapper');
-  const opt = {
-    margin: 0.5,
-    filename: 'CV.pdf',
-    image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { 
-      scale: 1,
-      windowHeight: '822px'
-     },
-    jsPDF: { 
-      unit: 'in', 
-      format: 'letter', 
-      orientation: 'portrait',
-   }
-  };
-
-  html2pdf().set(opt).from(CV).save();
-});
+const downloadButtonClickHandler = () => {
+  downloadButton.addEventListener('click', () => {
+    const CV = document.querySelector('.wrapper');
+    const opt = {
+      margin: 0.5,
+      filename: 'CV.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { 
+        scale: 1,
+       },
+      jsPDF: { 
+        unit: 'in', 
+        format: 'letter', 
+        orientation: 'portrait',
+     }
+    };
+  
+    html2pdf().set(opt).from(CV).save();
+  });
+}
 
 const saveToLocalStorage = () => {
   editingElements.forEach((elem) => {
@@ -54,6 +57,31 @@ const getFromLocalStorage = () => {
   })
 }
 
+const createRipple = (event) => {
+  const element = event.currentTarget;
+  const circle = document.createElement('span');
+  const diameter = Math.max(element.clientWidth, element.clientHeight);
+  const radius = diameter / 2;
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - (element.offsetLeft + radius)}px`;
+  circle.style.top = `${event.clientY - (element.offsetTop + radius)}px`;
+  circle.classList.add('ripple');
+  element.appendChild(circle);
+  circle.addEventListener('animationend', () => circle.remove());
+}
+
+const elementsClickHandler = () => {
+  editingElements.forEach((elem) => {
+    elem.classList.add('ripple_area');
+    elem.addEventListener('click', createRipple);
+  })
+}
+
 window.addEventListener('beforeunload', saveToLocalStorage);
 
-document.addEventListener('DOMContentLoaded', getFromLocalStorage);
+document.addEventListener('DOMContentLoaded', () => {
+  getFromLocalStorage();
+  animationHandler();
+  downloadButtonClickHandler();
+  elementsClickHandler();
+});
